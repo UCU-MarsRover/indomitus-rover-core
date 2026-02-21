d39349600e0720c12f5f81483bbd476997b145de
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-02-21 18:15:39 +0200

    Added .bashrc setuping with ros2 autorun and colcon autocomplete to the Dockerfile, bug fixed in the entrypoint.bash

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -5,7 +5,7 @@ set -e
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
 if [ -d /opt/ws/src ] && [ "$(ls -A  /opt/ws/src 2> /dev/null)" ]; then
-    if [ ! -f /opt/ws/install/setup.bash ] || [ /opt/ws/src -nt /opt/ws/install/setup.bash]; then
+    if [ ! -f /opt/ws/install/setup.bash ] || [ /opt/ws/src -nt /opt/ws/install/setup.bash ]; then
         echo "Building workspace..."
 
         rosdep install --from-paths src  --ignore-src -r -y || true
